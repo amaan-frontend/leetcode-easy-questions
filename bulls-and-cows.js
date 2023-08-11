8f08abd2f5ff8b78ea22e7030b7ad5de28bbc2e8
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-11 23:22:28 +0530

    Update bulls-and-cows.js

--- bulls-and-cows.js
+++ bulls-and-cows.js
@@ -1,1 +1,28 @@
+/**
+ * @param {string} secret
+ * @param {string} guess
+ * @return {string}
+ */
+var getHint = function (secret, guess) {
+    let bulls = 0;
+    let cows = 0;
+    const secretDigitsCount = new Array(10).fill(0);
+    const guessDigitsCount = new Array(10).fill(0);
+
+    for (let i = 0; i < secret.length; i++) {
+        if (secret[i] === guess[i]) {
+            bulls++;
+        } else {
+            secretDigitsCount[Number(secret[i])]++;
+            guessDigitsCount[Number(guess[i])]++;
+        }
+    }
+
+    for (let i = 0; i < 10; i++) {
+        cows += Math.min(secretDigitsCount[i], guessDigitsCount[i]);
+    }
+
+    return `${bulls}A${cows}B`;
+}
+
 
