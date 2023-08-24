4c733e92f3725da256e344660435c30c1e74e5dd
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-24 23:28:46 +0530

    Update and rename inert binary tree.js to invert binary tree.js

---
+++ invert binary tree.js
@@ -0,0 +1,25 @@
+
+class TreeNode {
+    constructor(val, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+function invertTree(root) {
+    if (root === null) {
+        return null;
+    }
+
+    // Swap the left and right subtrees
+    const temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    // Invert the left and right subtrees recursively
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+}
