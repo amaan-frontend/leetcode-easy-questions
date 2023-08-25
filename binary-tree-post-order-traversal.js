9d9482e067c99945d59b6fd03a241266571f19c6
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-25 23:21:30 +0530

    Update binary-tree-post-order-traversal.js

--- binary-tree-post-order-traversal.js
+++ binary-tree-post-order-traversal.js
@@ -1,1 +1,27 @@
+class TreeNode {
+    constructor(val, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
+function postorderTraversal(root) {
+    if (!root) {
+        return [];
+    }
+
+    const result = [];
+
+    // Traverse the left subtree
+    result.push(...postorderTraversal(root.left));
+
+    // Traverse the right subtree
+    result.push(...postorderTraversal(root.right));
+
+    // Visit the current node
+    result.push(root.val);
+
+    return result;
+}
+                                                                                        
