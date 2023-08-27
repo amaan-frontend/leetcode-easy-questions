0ee32c99e566e815eb9720b29fb9fbe1c42fec55
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-27 23:43:24 +0530

    Update valid-perfect-square.js

--- valid-perfect-square.js
+++ valid-perfect-square.js
@@ -1,1 +1,13 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function (num) {
+    for (let i = 1; i * i <= num; i++) {
+        if (i * i === num) {
+            return true;
+        }
+    }
+    return false;
+}
 
