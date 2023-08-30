77a53dfbb47fb058e1eb42de4fa13da7b08b57e2
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-30 23:51:20 +0530

    Update maximum-depth-of-n-ary-tree.js

--- maximum-depth-of-n-ary-tree.js
+++ maximum-depth-of-n-ary-tree.js
@@ -1,1 +1,21 @@
+class Node {
+    constructor(val, children) {
+        this.val = val;
+        this.children = children || [];
+    }
+}
 
+function maxDepth(root) {
+    if (!root) {
+        return 0;
+    }
+
+    let maxChildDepth = 0;
+    for (const child of root.children) {
+        const childDepth = maxDepth(child);
+        maxChildDepth = Math.max(maxChildDepth, childDepth);
+    }
+
+    return maxChildDepth + 1;
+}
+                          
