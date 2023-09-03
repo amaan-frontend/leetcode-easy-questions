8d2089b7540f11bf5fa271a26fad2746269e5ed0
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-03 23:27:32 +0530

    Update subtree-of-another-tree.js

--- subtree-of-another-tree.js
+++ subtree-of-another-tree.js
@@ -1,1 +1,28 @@
+function isSubtree(root, subRoot) {
+    if (!root) {
+        return false;
+    }
 
+    if (isSameTree(root, subRoot)) {
+        return true;
+    }
+
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+}
+
+function isSameTree(tree1, tree2) {
+    if (!tree1 && !tree2) {
+        return true;
+    }
+
+    if (!tree1 || !tree2) {
+        return false;
+    }
+
+    if (tree1.val !== tree2.val) {
+        return false;
+    }
+
+    return isSameTree(tree1.left, tree2.left) && isSameTree(tree1.right, tree2.right);
+}
+                                                                                            
