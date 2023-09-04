91c5b1f5b2c3232375a090d8c6a4b026e11f66c4
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-04 22:38:29 +0530

    Update third-maximum-number.js

--- third-maximum-number.js
+++ third-maximum-number.js
@@ -1,1 +1,33 @@
+#include <stdio.h>
+
+int utopianTree(int n) {
+    int height = 1;
+    
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 1) {
+            // Spring cycle: Double the height
+            height *= 2;
+        } else {
+            // Summer cycle: Increase height by 1 meter
+            height += 1;
+        }
+    }
+    
+    return height;
+}
+
+int main() {
+    int t; // Number of test cases
+    scanf("%d", &t);
+    
+    while (t--) {
+        int n; // Number of growth cycles
+        scanf("%d", &n);
+        
+        int result = utopianTree(n);
+        printf("%d\n", result);
+    }
+    
+    return 0;
+}
 
