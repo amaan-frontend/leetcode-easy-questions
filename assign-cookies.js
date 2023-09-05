445ad2342317898eee2df494aec432d6daa097eb
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-05 22:22:16 +0530

    Update assign-cookies.js

--- assign-cookies.js
+++ assign-cookies.js
@@ -1,1 +1,25 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function (g, s) {
+    // Sort the arrays in ascending order.
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
 
+    let contentChildren = 0;
+    let i = 0; // Index for greed factors
+    let j = 0; // Index for cookie sizes
+
+    while (i < g.length && j < s.length) {
+        if (s[j] >= g[i]) {
+            // If the current cookie size can satisfy the current child's greed factor, assign the cookie to the child.
+            contentChildren++;
+            i++;
+        }
+        j++; // Move to the next cookie, regardless of whether it was assigned or not.
+    }
+
+    return contentChildren;
+};
