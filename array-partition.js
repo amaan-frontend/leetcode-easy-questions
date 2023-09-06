a783b76665538acf581dd37f112f15c82cf45417
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-06 23:38:59 +0530

    Update array-partition.js

--- array-partition.js
+++ array-partition.js
@@ -1,1 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function (nums) {
+    nums.sort((a, b) => a - b);
 
+    let sum = 0;
+
+    // Pair the elements at even indices with elements at odd indices
+    for (let i = 0; i < nums.length; i += 2) {
+        sum += nums[i];
+    }
+
+    return sum;
+};
