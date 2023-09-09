f89c051425650ee8b4a28306a66cd779489bbd9d
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-09 23:56:56 +0530

    Update set-mismatch.js

--- set-mismatch.js
+++ set-mismatch.js
@@ -1,1 +1,21 @@
+function findErrorNums(nums) {
+    const seen = new Set();
+    let duplicate, missing;
 
+    for (const num of nums) {
+        if (seen.has(num)) {
+            duplicate = num;
+        }
+        seen.add(num);
+    }
+
+    for (let i = 1; i <= nums.length; i++) {
+        if (!seen.has(i)) {
+            missing = i;
+            break;
+        }
+    }
+
+    return [duplicate, missing];
+}
+                                                                                                                        
