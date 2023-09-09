fb6fc3df201d731299a7e610aa4f338cac20b143
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-09 23:57:43 +0530

    Update maximum-product-of-three-numbers.js

--- maximum-product-of-three-numbers.js
+++ maximum-product-of-three-numbers.js
@@ -1,1 +1,9 @@
+function maximumProduct(nums) {
+    nums.sort((a, b) => b - a); // Sort the array in descending order
+    const n = nums.length;
+    const option1 = nums[0] * nums[1] * nums[2]; // Product of three largest elements
+    const option2 = nums[0] * nums[n - 1] * nums[n - 2]; // Product of largest and two smallest elements
 
+    return Math.max(option1, option2);
+}
+                            
