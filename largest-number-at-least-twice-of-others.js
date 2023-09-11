643edecb5b82bc351d5d110e1494b5200cedc110
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-11 22:19:54 +0530

    Update largest-number-at-least-twice-of-others.js

--- largest-number-at-least-twice-of-others.js
+++ largest-number-at-least-twice-of-others.js
@@ -1,1 +1,26 @@
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var dominantIndex = function (nums) {
+    let maxIndex = 0;
+    let maxNum = nums[0];
+
+    // Find the maximum element and its index
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > maxNum) {
+            maxNum = nums[i];
+            maxIndex = i;
+        }
+    }
+
+    // Check if the condition is satisfied
+    for (let i = 0; i < nums.length; i++) {
+        if (i !== maxIndex && nums[i] * 2 > maxNum) {
+            return -1;
+        }
+    }
+
+    return maxIndex;
+};
