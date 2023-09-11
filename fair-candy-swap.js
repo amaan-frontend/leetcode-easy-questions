d682cbdc5cf500e61e7ba980f01f6736d88930e9
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-11 22:20:41 +0530

    Update fair-candy-swap.js

--- fair-candy-swap.js
+++ fair-candy-swap.js
@@ -1,1 +1,25 @@
+/**
+ * @param {number[]} aliceSizes
+ * @param {number[]} bobSizes
+ * @return {number[]}
+ */
+var fairCandySwap = function (aliceSizes, bobSizes) {
+    function candyExchange(aliceSizes, bobSizes) {
+        const totalAlice = aliceSizes.reduce((acc, val) => acc + val, 0);
+        const totalBob = bobSizes.reduce((acc, val) => acc + val, 0);
+
+        const targetTotal = (totalAlice + totalBob) / 2;
+
+        for (let i = 0; i < aliceSizes.length; i++) {
+            for (let j = 0; j < bobSizes.length; j++) {
+                const newAliceTotal = totalAlice - aliceSizes[i] + bobSizes[j];
+                const newBobTotal = totalBob - bobSizes[j] + aliceSizes[i];
+
+                if (newAliceTotal === targetTotal && newBobTotal === targetTotal) {
+                    return [aliceSizes[i], bobSizes[j]];
+                }
+            }
+        }
+    }
+}
 
