2e3c88644506b34d5c3569ff8387f41a1b244b5f
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-12 23:07:22 +0530

    Update minimum-index-sum-of-two-indexes.js

--- minimum-index-sum-of-two-indexes.js
+++ minimum-index-sum-of-two-indexes.js
@@ -1,1 +1,28 @@
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function (list1, list2) {
+    const commonStrings = [];
+    const indexSumMap = new Map();
+    let minIndexSum = Infinity;
+
+    for (let i = 0; i < list1.length; i++) {
+        for (let j = 0; j < list2.length; j++) {
+            if (list1[i] === list2[j]) {
+                const indexSum = i + j;
+                if (indexSum < minIndexSum) {
+                    minIndexSum = indexSum;
+                    commonStrings.length = 0; // Reset the array
+                }
+                if (indexSum === minIndexSum) {
+                    commonStrings.push(list1[i]);
+                }
+            }
+        }
+    }
+
+    return commonStrings;
+}
 
