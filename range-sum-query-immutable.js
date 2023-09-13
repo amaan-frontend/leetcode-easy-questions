064e6d6115723802972d81538dab3e1f38033f78
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-13 22:33:16 +0530

    Create range-sum-query-immutable.js

---
+++ range-sum-query-immutable.js
@@ -0,0 +1,13 @@
+class NumArray {
+    constructor(nums) {
+        this.prefixSum = [0];
+        
+        for (let i = 0; i < nums.length; i++) {
+            this.prefixSum[i+1] = this.prefixSum[i] + nums[i];
+        }
+    }
+    
+    sumRange(left, right) {
+        return this.prefixSum[right + 1] - this.prefixSum[left];
+    }
+}
