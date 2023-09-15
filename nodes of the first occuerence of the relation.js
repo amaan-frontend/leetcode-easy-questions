4a45de2449664f55f821d04c016ddd713fb1a649
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-15 23:46:44 +0530

    Update nodes of the first occuerence of the relation.js

--- nodes of the first occuerence of the relation.js
+++ nodes of the first occuerence of the relation.js
@@ -1,1 +1,10 @@
 
+ if (needle === "") return 0;
+    
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        if (haystack.slice(i, i + needle.length) === needle) {
+            return i;
+        }
+    }
+    
+    return -1;
