60b662e1a6d1bba13939a4a26c0d138161236611
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-17 23:39:50 +0530

    Update degree of an array.js

--- degree of an array.js
+++ degree of an array.js
@@ -1,1 +1,28 @@
 
+const frequency = {};
+    const firstOccurrence = {};
+    let maxFrequency = 0;
+    let minLength = Infinity;
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+
+        if (firstOccurrence[num] === undefined) {
+            firstOccurrence[num] = i;
+        }
+
+        frequency[num] = frequency[num] + 1 || 1;
+
+        if (frequency[num] > maxFrequency) {
+            maxFrequency = frequency[num];
+        }
+    }
+
+    for (const num in frequency) {
+        if (frequency[num] === maxFrequency) {
+            const length = firstOccurrence[num] !== undefined ? i - firstOccurrence[num] + 1 : 1;
+            minLength = Math.min(minLength, length);
+        }
+    }
+
+    return minLength;
