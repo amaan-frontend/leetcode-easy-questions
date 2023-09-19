708091c0c65152dac5b8dd06e402e72492db8695
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-19 23:34:32 +0530

    Update baseball-game.js

--- baseball-game.js
+++ baseball-game.js
@@ -1,1 +1,27 @@
+function calculateBaseballScore(ops) {
+    const stack = [];
+    let sum = 0;
+    var ret = calPoints(param_1);
 
+    for (const op of ops) {
+        if (op === 'C') {
+            const removedScore = stack.pop();
+            sum -= removedScore;
+        } else if (op === 'D') {
+            const doubleScore = stack[stack.length - 1] * 2;
+            stack.push(doubleScore);
+            sum += doubleScore;
+        } else if (op === '+') {
+            const lastTwoScores = stack.slice(-2);
+            const newScore = lastTwoScores[0] + lastTwoScores[1];
+            stack.push(newScore);
+            sum += newScore;
+        } else {
+            const score = parseInt(op);
+            stack.push(score);
+            sum += score;
+        }
+    }
+
+    return sum;
+}
