dcbff210ca77d82f6ec3e18b70fb7db6112858eb
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-21 23:07:38 +0530

    Update repeated-substring.js

--- repeated-substring.js
+++ repeated-substring.js
@@ -1,1 +1,25 @@
-
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var repeatedSubstringPattern = function(s) {
+    const n = s.length;
+    
+    for (let len = 1; len <= n / 2; len++) {
+        if (n % len === 0) {
+            const substring = s.slice(0, len);
+            let valid = true;
+            for (let i = len; i < n; i += len) {
+                if (s.slice(i, i + len) !== substring) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                return true;
+            }
+        }
+    }
+    
+    return false;
+}
