e4235a89e21570b9b48e4e465fb825b7d52447dd
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-22 23:49:26 +0530

    Update arranging-coins.js

--- arranging-coins.js
+++ arranging-coins.js
@@ -1,1 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins = function (n) {
 
+    let k = 1;
+    while (k * (k + 1) / 2 <= n) {
+        k++;
+    }
+    return k - 1;
+
+
+};
