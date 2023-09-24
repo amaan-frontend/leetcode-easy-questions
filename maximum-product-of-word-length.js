e28721e4cbea6c905b6e7c210d0d215b483cf5af
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-09-24 21:12:06 +0530

    Update maximum-product-of-word-length.js

--- maximum-product-of-word-length.js
+++ maximum-product-of-word-length.js
@@ -1,1 +1,22 @@
+ function hasCommonLetters(word1, word2) {
+        const set1 = new Set(word1);
+        const set2 = new Set(word2);
+        for (const char of set1) {
+            if (set2.has(char)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    let maxProduct = 0;
+
+    for (let i = 0; i < words.length; i++) {
+        for (let j = i + 1; j < words.length; j++) {
+            if (!hasCommonLetters(words[i], words[j])) {
+                maxProduct = Math.max(maxProduct, words[i].length * words[j].length);
+            }
+        }
+    }
+
+    return maxProduct;
