0ae6af28f85c72fc250ff861e815df79130cf5c6
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-04 23:53:37 +0530

    Update majority-element-2.js

--- majority-element-2.js
+++ majority-element-2.js
@@ -1,1 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var majorityElement = function(nums) {
+  let majorityElement = null;
+  let count = 0;
+
+  // Find a candidate majority element.
+  for (const num of nums) {
+    if (num === majorityElement) {
+      count++;
+    } else if (count === 0) {
+      majorityElement = num;
+      count = 1;
+    } else {
+      count--;
+    }
+  }
+
+  // Verify that the candidate majority element is actually the majority element.
+  count = 0;
+  for (const num of nums) {
+    if (num === majorityElement) {
+      count++;
+    }
+  }
+
+  if (count > nums.length / 3) {
+    return majorityElement;
+  } else {
+    return null;
+  }
+}
 
