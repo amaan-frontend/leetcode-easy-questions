d38904eb2cb3963841352563405990f1d5e9b51a
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-08 23:52:53 +0530

    Update fibonacci-number.js

--- fibonacci-number.js
+++ fibonacci-number.js
@@ -1,1 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if (n <= 1) {
+        return n;
+    }
 
+    let fibNMinus2 = 0;
+    let fibNMinus1 = 1;
+    let fibN = 0;
+
+    for (let i = 2; i <= n; i++) {
+        fibN = fibNMinus1 + fibNMinus2;
+        fibNMinus2 = fibNMinus1;
+        fibNMinus1 = fibN;
+    }
+
+    return fibN;
+};
