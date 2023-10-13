3ff1d81ee7d85e76243a37d2eb55531d974ef8df
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-13 23:53:20 +0530

    Update two-sum-4-of-a-bst.js

--- two-sum-4-of-a-bst.js
+++ two-sum-4-of-a-bst.js
@@ -1,1 +1,26 @@
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
 
+function findTarget(root, k) {
+    const encountered = new Set();
+    
+    function dfs(node) {
+        if (!node) return false;
+        
+        const complement = k - node.val;
+        
+        if (encountered.has(complement)) {
+            return true;
+        }
+        
+        encountered.add(node.val);
+        
+        return dfs(node.left) || dfs(node.right);
+    }
+    
+    return dfs(root);
+}
