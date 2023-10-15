d523a85914f8792c7c95579b56c44732bd5f4c56
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-15 23:42:35 +0530

    Update unique-morse-code.js

--- unique-morse-code.js
+++ unique-morse-code.js
@@ -1,1 +1,20 @@
+function uniqueMorseRepresentations(words) {
+    const morseCodes = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
+    
+    function wordToMorse(word) {
+        let morse = '';
+        for (let char of word) {
+            morse += morseCodes[char.charCodeAt(0) - 'a'.charCodeAt(0)];
+        }
+        return morse;
+    }
+    
+    const transformations = new Set();
+    
+    for (let word of words) {
+        transformations.add(wordToMorse(word));
+    }
+    
+    return transformations.size;
+}
 
