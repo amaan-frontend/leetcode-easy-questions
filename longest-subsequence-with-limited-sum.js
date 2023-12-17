ed7d7b6c55bb329770f48233aebbcc6e94b303a5
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-12-17 22:40:19 +0530

    Update longest-subsequence-with-limited-sum.js

--- longest-subsequence-with-limited-sum.js
+++ longest-subsequence-with-limited-sum.js
@@ -1,1 +1,20 @@
 
+var answerQueries = function (nums, queries) {
+    nums = nums.sort((a, b) => a - b);
+    const output = [];
+
+    for (let i = 0; i < queries.length; i++) {
+        const target = queries[i];
+        let sum = 0;
+        for (let j = 0; j < nums.length; j++) {
+            sum += nums[j];
+            if (sum > target) {
+                output.push(j)
+                break;
+            }
+        }
+        if (output[i] === undefined) output.push(nums.length);
+    }
+
+    return output;
+};
