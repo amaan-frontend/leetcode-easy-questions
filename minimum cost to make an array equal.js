93f4f8d79f5dcdc08a23ecd2967367b8a82a51ae
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-12-18 23:12:27 +0530

    Update minimum cost to make an array equal.js

--- minimum cost to make an array equal.js
+++ minimum cost to make an array equal.js
@@ -1,1 +1,46 @@
 
+var minCost = (nums, cost) => {
+    const min = findCost(nums, cost, 1); /* Time O(N) | Space O(1) */
+
+    return binarySearch(nums, cost, min);/* Time O(N * log(N)) | Space O(1) */
+}
+
+const findCost = (nums, cost, x, min = 0) => {
+    for (let i = 0; i < nums.length; i++) {/* Time O(N) */
+        const num = Math.abs(nums[i] - x);
+
+        min += (num * cost[i]);
+    }
+
+    return min;
+}
+
+const binarySearch = (nums, cost, min) => {
+    let [left, right] = getPointers(nums);/* Time O(N) */
+
+    while (left < right) {/* Time O(log(N)) */
+        const mid = ((left + right) >> 1);
+        const [x, y] =      /* Time O(N) */
+            [findCost(nums, cost, mid), findCost(nums, cost, (mid + 1))];
+
+        min = Math.min(x, y);
+
+        const isGreater = (y <= x)
+        if (isGreater) left = (mid + 1);
+
+        const isLess = (x < y)
+        if (isLess) right = mid;
+    }
+
+    return min;
+}
+
+const getPointers = (nums) => {
+    let [left, right] = [1, 1000000];
+
+    for (const num of nums) {/* Time O(N) */
+        [left, right] = [Math.min(num, left), Math.max(num, right)];
+    }
+
+    return [left, right];
+}
