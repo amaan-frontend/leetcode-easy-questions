43c08a823efe6fc718a9dd3f9d79086a88de8bb8
frontender <134868734+amaan-frontend@users.noreply.github.com>
2024-01-02 23:48:54 +0530

    Update find-center-of-star-graph.js

--- find-center-of-star-graph.js
+++ find-center-of-star-graph.js
@@ -1,1 +1,10 @@
+const findCenter = function (edges) {
+    const dict = {};
 
+    edges.map(([v1, v2]) => {
+        dict[v1] = dict[v1] ? dict[v1] + 1 : 1;
+        dict[v2] = dict[v2] ? dict[v2] + 1 : 1;
+    })
+
+    return Object.entries(dict).find(([k, v]) => v > 1)[0]
+};
