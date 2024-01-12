d4567ade71226abb531ab003ec5f0d9c9b40c0f1
frontender <134868734+amaan-frontend@users.noreply.github.com>
2024-01-12 23:31:15 +0530

    Create determine if string halves are alike.js

---
+++ determine if string halves are alike.js
@@ -0,0 +1,19 @@
+var halvesAreAlike = function (s) {
+    let arr = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    let a = s.slice(0, s.length / 2);
+    let b = s.slice(s.length / 2, s.length);
+
+    let countA = 0;
+    let countB = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let ia = 0; ia < a.length; ia++) {
+            if (a[ia] == arr[i]) countA++;
+        }
+        for (let ib = 0; ib < b.length; ib++) {
+            if (b[ib] == arr[i]) countB++;
+        }
+    }
+
+    return (countA == countB);
+};
