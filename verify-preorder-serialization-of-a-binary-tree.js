59d9921541440ef173b228269a8a6e98c11709bd
frontender <134868734+amaan-frontend@users.noreply.github.com>
2024-01-15 23:00:17 +0530

    Update verify-preorder-serialization-of-a-binary-tree.js

--- verify-preorder-serialization-of-a-binary-tree.js
+++ verify-preorder-serialization-of-a-binary-tree.js
@@ -1,1 +1,14 @@
-
+var isValidSerialization = function (preorder) {
+    let vacancy = 1
+    preorder = preorder.split(',')
+    for (let node of preorder) {
+        vacancy = vacancy - 1
+        if (vacancy < 0) { 
+            return false
+        }
+        if (node != '#') {
+            vacancy = vacancy + 2 
+        }
+    }
+    return vacancy == 0
+};
